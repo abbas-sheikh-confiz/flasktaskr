664fa92db1a259ce3d38f685a37c5750903d77e0
Abbas Sheikh <abbas.sheikh@confiz.com>
2017-08-08 19:01:22 +0500

    Updated script to create DB

--- db_create.py
+++ db_create.py
@@ -1,15 +1,22 @@
-# project/db_create.py
+# db_create.py
 
-from views import db
-from models import Task
 from datetime import date
+
+
+from project import db
+from project.models import Task, User
+
 
 # create the database and the db table
 db.create_all()
 
 # insert data
-# db.session.add(Task("Finish this tutorial", date(2017, 7, 22), 10, 1))
-# db.session.add(Task("Finish Real Python", date(2017, 9, 21), 10, 1))
+db.session.add(
+	User("admin", "admin@gmail.com", "lahore", "admin")
+	)
+
+db.session.add(Task("Finish this tutorial", date(2017, 10, 22), 10, date(2017, 8, 8), 1, 1))
+db.session.add(Task("Finish Real Python", date(2017, 10, 21), 10, date(2017, 8, 8), 1, 1))
 
 # commit the changes
 db.session.commit()
